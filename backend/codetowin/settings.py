8bf35877c64c0abb94b2118f5119a4ac2d612e84
¨david <idowuasabidavid@gmail>
2026-07-01 20:48:14 +0200

    fix: use database cache to fix otp cross-worker issue

--- backend/codetowin/settings.py
+++ backend/codetowin/settings.py
@@ -110,6 +110,13 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'django_cache_table',
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
